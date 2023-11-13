41e42bd09fda90d755b504f23921b8acf8476cd3
S0619 <S0619@22S0619>
2023-11-13 12:26:11 +0900

    setting

--- tigaimasu/src/main/java/com/example/demo/controller/settingcontroller.java
+++ tigaimasu/src/main/java/com/example/demo/controller/settingcontroller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Joincontroller {
+public class settingcontroller {
 
 	@RequestMapping(path = "/join", method = RequestMethod.GET)
 	public String getJoinPage(Model model) {
@@ -20,36 +20,16 @@ public class Joincontroller {
 			@RequestParam("email") String email,
 			@RequestParam("password") String password,
 			@RequestParam("birthday") String birthday,
-			@RequestParam("gender") String gender,
 			Model model) {
 
 		// フォームデータを必要に応じて処理（例: データベースへ保存）
-
-		// 性別を数値に変換
-		int genderValue;
-		switch (gender) {
-		case "male":
-			genderValue = 1;
-			break;
-		case "female":
-			genderValue = 2;
-			break;
-		case "other":
-			genderValue = 3;
-			break;
-		default:
-			genderValue = 0; // エラーなどに対応する場合は、適切な処理を追加
-		}
-
-		// データベースに登録する処理を追加
 
 		model.addAttribute("username", username);
 		model.addAttribute("email", email);
 		model.addAttribute("password", password);
 		model.addAttribute("birthday", birthday);
-		model.addAttribute("gender", genderValue); // 数値で保存
 
 		// 確認ページや他のページにリダイレクト
-		return "confirmation";
+		return "join";
 	}
 }
