84b8b9781465afd3e288ca42a37865b66a069f5d
S0634 <S0634@22S0634>
2023-11-13 11:36:53 +0900

    最初にぶち込みます

---
+++ tigaimasu/src/main/java/com/example/demo/TigaimasuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigaimasuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigaimasuApplication.class, args);
+	}
+
+}
