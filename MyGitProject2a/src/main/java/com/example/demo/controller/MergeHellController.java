674677588c75f4eb1e0fb5f470bb23081f79a663
S0634 <S0634@22S0634>
2023-11-14 15:03:49 +0900

    54

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+
+	void kadaiA() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("現在" + i + "回目のループです");
+		}
+	}
+}
