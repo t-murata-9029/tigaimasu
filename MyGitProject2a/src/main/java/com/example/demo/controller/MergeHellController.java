c6f787241c243ddba9480c9a9037fa605f0e0761
S0635 <S0635@22S0635>
2023-11-14 15:05:47 +0900

    fuck

---
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+		kadaiB();
+		kadaiC();
+	}
+
+	void kadaiA() {
+		int a, b, c;
+		a = 50;
+		b = 75;
+		c = a + b;
+		System.out.println(c);
+
+	}
+}
