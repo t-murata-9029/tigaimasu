b6f4a71bebb8f7c7a661beb01130f26c175675a2
S0619 <S0619@22S0619>
2023-11-16 10:42:39 +0900

    ルーム作成,フォロー一覧（DB作成後編集予定）

---
+++ tigaimasu/src/main/java/com/example/demo/controller/FollowController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+////import java.util.List;
+////
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.stereotype.Controller;
+////import org.springframework.ui.Model;
+////import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+public class FollowController {
+
+	//	@Autowired
+	//	private UserService userService;
+	//
+	//	@GetMapping("/follow-list")
+	//	public String showFollowList(Model model) {
+	//		// 現在のユーザーのフォロワー一覧をDBから取得
+	//		List<User> followers = userService.getCurrentUsersFollowers();
+	//
+	//		model.addAttribute("follow", follow);
+	//		return "follow-list";
+	//	}
+}
