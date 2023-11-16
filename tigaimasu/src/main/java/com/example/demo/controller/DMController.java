6cf04167b03c829b7f178428f425bab67febfe6c
S0619 <S0619@22S0619>
2023-11-16 11:01:22 +0900

    DM一覧（DB作成後編集予定）

---
+++ tigaimasu/src/main/java/com/example/demo/controller/DMController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+////import java.util.List;
+////
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.stereotype.Controller;
+////import org.springframework.ui.Model;
+////import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+public class DMController {
+
+	//	@Autowired
+	//	private UserService userService;
+	//
+	//	@GetMapping("/dms-list")
+	//	public String showdmsList(Model model) {
+	//		// 現在のユーザーのフォロワー一覧をDBから取得
+	//		List<User> dms = userService.getCurrentUsersdms();
+	//
+	//		model.addAttribute("dms", dms);
+	//		return "dms-list";
+	//	}
+}
