459ea0a284e829839aa482f6a87da73d342ea39b
S0634 <S0634@22S0634>
2023-11-20 10:01:01 +0900

    ieeeeeeeeeeeeeeeeeeeeeeeeeei

--- tigaimasu/src/main/java/com/example/demo/controller/LoginController.java
+++ tigaimasu/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,16 +1,36 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.service.LoginService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
 	public String input1(HttpServletRequest request) {
+		String sql = "SELECT * FROM users WHERE user_id = 'sample_id'";
+
+		Map<String, Object> oneUser = jdbcTemplate.queryForMap(sql);
+
+		for (Entry<String, Object> a : oneUser.entrySet()) {
+			System.out.println(a.getValue());
+		}
+
+		LoginService loginService = new LoginService();
+		loginService.LoginCheck(null, null);
 		return "login";
 	}
 
--- tigaimasu/src/main/java/com/example/demo/service/LoginService.java
+++ tigaimasu/src/main/java/com/example/demo/service/LoginService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.LoginDao;
+
 public class LoginService {
 	public Boolean LoginCheck(String userId, String userPassward) {
 
 		//DBにSQLぶち込んでその結果見る
+
+		LoginDao loginDao = new LoginDao();
+		loginDao.getUserInfo();
 
 		return false;
 	}
