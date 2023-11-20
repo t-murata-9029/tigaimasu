a499ad815527d8e86be065673adcf97bdbf673fd
S0619 <S0619@22S0619>
2023-11-20 12:36:47 +0900

    引っ越し

---
+++ tigaimasu2/src/main/java/com/example/demo/controller/NoticeController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+////import java.util.List;
+////
+////import org.springframework.beans.factory.annotation.Autowired;
+////import org.springframework.stereotype.Controller;
+////import org.springframework.ui.Model;
+////import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+public class NoticeController {
+
+	//	@Autowired
+	//	private UserService userService;
+	//
+	//	@GetMapping("/notice-list")
+	//	public String showNoticeList(Model model) {
+	//		// 現在のユーザーのフォロワー一覧をDBから取得
+	//		List<User> notice = userService.getCurrentUsersNotice();
+	//
+	//		model.addAttribute("notice", notice);
+	//		return "notice-list";
+	//	}
+}
