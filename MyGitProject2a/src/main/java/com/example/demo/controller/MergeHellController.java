74f11a1f63b8104e81630f1d83625a8885b6ade1
S0634 <S0634@22S0634>
2023-11-20 09:45:27 +0900

    FUCK

--- MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
+++ MyGitProject2a/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -1,15 +1,31 @@
 package com.example.demo.controller;
 
+import java.util.Random;
+
 public class MergeHellController {
 	public static void main(String[] args) {
 		kadaiA();
 		kadaiB();
-		kadaiC();
+		//kadaiC();
 	}
 
 	static void kadaiA() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("現在" + i + "回目のループです");
 		}
+	}
+
+	static void kadaiB() {
+		Random rnd = new Random();
+		System.out.println(rnd.nextInt() % 100 + 1 + "がでました。");
+	}
+
+	static void kadaiC() {
+		int a = 50;
+		int b = 75;
+
+		int c = a + b;
+
+		System.out.println(c);
 	}
 }
